4647c8328324e8930993c5bcc64de09bdf1a8124
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-30 15:16:51 -0300

    Testing Connection using Driver manager

---
+++ rodrigocury/src/main/java/br/dev/rodrigocury/TestaConexão.java
@@ -0,0 +1,30 @@
+package br.dev.rodrigocury;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexão {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC";
+		String user = "root";
+		String env_key = "MYSQL_PASS";
+        String password = System.getenv(env_key);
+		
+
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			
+			
+			
+			connection.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}

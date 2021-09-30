fb69df9a36f4b383b28fbb64c407061fed0e5f61
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-30 18:33:48 -0300

    Using mChange to make Connection Pool

--- rodrigocury/src/main/java/br/dev/rodrigocury/main/ConnectioFactory.java
+++ rodrigocury/src/main/java/br/dev/rodrigocury/main/ConnectioFactory.java
@@ -1,16 +1,34 @@
 package br.dev.rodrigocury.main;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectioFactory {
+	
+	public static DataSource dataSource;
+
+	private static String url = "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC";
+	private static String user = "root";
+	private static String env_key = "MYSQL_PASS";
+	private static String password = System.getenv(env_key);
+	
+	static {
+		ComboPooledDataSource cpds = new ComboPooledDataSource();
+		cpds.setJdbcUrl(url);
+		cpds.setUser(user);
+		cpds.setPassword(password);
+		dataSource = cpds;
+	}
+	
 	public static Connection create() throws SQLException {
-		String url = "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC";
-		String user = "root";
-		String env_key = "MYSQL_PASS";
-        String password = System.getenv(env_key);
-        
-        return DriverManager.getConnection(url, user, password);
+        return dataSource.getConnection();
+	}
+	
+	public static DataSource getDataSource() {
+		return dataSource;
 	}
 }

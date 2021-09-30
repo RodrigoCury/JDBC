badd024608bf43a92e89fea1664a14d72eb753a1
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-30 17:50:10 -0300

    More Controll over transaction

--- rodrigocury/src/main/java/br/dev/rodrigocury/main/TestaInsercaoComParametro.java
+++ rodrigocury/src/main/java/br/dev/rodrigocury/main/TestaInsercaoComParametro.java
@@ -3,31 +3,45 @@ package br.dev.rodrigocury.main;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class TestaInsercaoComParametro {
 
-	public static void main(String[] args) {
-		String nome = "Mouse G305";
-		String descricao = "Mouse Sem fio Lightning Logitech G305'";
+	public static void main(String[] args) throws SQLException {
 		
-		try (Connection connection = ConnectioFactory.create()){
+		Connection connection = ConnectioFactory.create();
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", PreparedStatement.RETURN_GENERATED_KEYS);
+		
+		try {
+			connection.setAutoCommit(false);
 			
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", PreparedStatement.RETURN_GENERATED_KEYS);
-			stm.setString(1, nome);
-			stm.setString(2, descricao);
+			extracted("Tv Smart", "LG 45" , stm);
+			extracted("Radin", "Bateria", stm);
+			
+			connection.commit();
+		} catch (Exception e) {
+			e.getStackTrace();
+			System.out.println("Rollback Aconteceu!");
+			connection.rollback();
+		} finally {
+			stm.close();
+			connection.close();
+		}
+	}
 
-			stm.execute();
-			
-			ResultSet rst = stm.getGeneratedKeys();
+	private static void extracted(String nome, String descricao, PreparedStatement stm) throws SQLException {
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+
+		stm.execute();
+		
+		try (ResultSet rst = stm.getGeneratedKeys()){
 			while (rst.next()){
 				Integer id= rst.getInt(1);
 				System.out.println(String.format("Created ID: %d", id));
-			}
-			
-			
-		} catch (Exception e) {
-			e.getStackTrace();
+			}			
 		}
+				
 	}
 
 }

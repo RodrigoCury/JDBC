534676001089238aa8860de9f091aacc44531ae3
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-30 15:57:44 -0300

    Applying DELETE

---
+++ rodrigocury/src/main/java/br/dev/rodrigocury/main/TestaRemocao.java
@@ -0,0 +1,27 @@
+package br.dev.rodrigocury.main;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class TestaRemocao {
+
+	public static void main(String[] args) {
+		Integer id = 4;
+		
+		try (Connection connection = ConnectioFactory.create()) {
+
+			Statement stm = connection.createStatement();
+			
+			stm.execute(String.format("DELETE FROM PRODUTO WHERE ID = %d", id));
+
+			Integer modifiedLines = stm.getUpdateCount();
+					
+			System.out.println(String.format("Linhas Modificadas: %d", modifiedLines));
+			
+
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+	}
+
+}

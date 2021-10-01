0cfaace4907c6478a29bf13942522aa17af2250d
Rodrigo Cury <rodrigocury@ufu.br>
2021-10-01 00:07:01 -0300

    Testing DAO

---
+++ rodrigocury/src/main/java/br/dev/rodrigocury/main/TestaProdutoDAO.java
@@ -0,0 +1,33 @@
+package br.dev.rodrigocury.main;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.dev.rodrigocury.DAO.ProdutoDAO;
+
+public class TestaProdutoDAO {
+
+	public static void main(String[] args) {
+		
+		
+		try (Connection c = ConnectioFactory.create()){
+			
+			ProdutoDAO pd = new ProdutoDAO(c);
+			
+			pd.salvarProduto("Banana", "de Pijama");
+			
+			pd.listar().forEach(System.out::println);
+			
+			pd.removeProduto(12);
+			
+			pd.listar().forEach(System.out::println);
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}

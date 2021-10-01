a20f86ab6132efcbfd73e72b40559c658ef4a085
Rodrigo Cury <rodrigocury@ufu.br>
2021-10-01 14:43:14 -0300

    Updated with the right Methods

--- rodrigocury/src/main/java/br/dev/rodrigocury/contollers/CategoriaController.java
+++ rodrigocury/src/main/java/br/dev/rodrigocury/contollers/CategoriaController.java
@@ -1,16 +1,23 @@
 package br.dev.rodrigocury.contollers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import br.dev.rodrigocury.DAO.CategoriaDAO;
+import br.dev.rodrigocury.factories.ConnectioFactory;
 import br.dev.rodrigocury.models.Categoria;
 
 public class CategoriaController {
+	
+	private CategoriaDAO categoriaDAO;
+	
+	public CategoriaController() {
+		categoriaDAO = new CategoriaDAO(ConnectioFactory.create());
+	}
 
 	public List<Categoria> listar() {
-		List<Categoria> categorias = 
-				new ArrayList<Categoria>();
-		categorias.add(new Categoria("Categoria de teste", 1)); 
+		
+		List<Categoria> categorias = categoriaDAO.listar();
+		
 		return categorias;
 	}
 }
--- rodrigocury/src/main/java/br/dev/rodrigocury/contollers/ProdutoController.java
+++ rodrigocury/src/main/java/br/dev/rodrigocury/contollers/ProdutoController.java
@@ -1,29 +1,35 @@
 package br.dev.rodrigocury.contollers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import br.dev.rodrigocury.DAO.ProdutoDAO;
+import br.dev.rodrigocury.factories.ConnectioFactory;
 import br.dev.rodrigocury.models.Produto;
 
 public class ProdutoController {
+	
+	private ProdutoDAO produtoDAO;
+	
+	public ProdutoController() {
+		this.produtoDAO = new ProdutoDAO(ConnectioFactory.create());
+	}
 
 	public void deletar(Integer id) {
-		System.out.println("Deletando produto");
+		produtoDAO.removeProduto(id);
 	}
 
 	public void salvar(Produto produto) {
-		System.out.println("Salvando produto");
+		produtoDAO.salvarProduto(produto);
 	}
 
 	public List<Produto> listar() {
-		List<Produto> produtos = 
-				new ArrayList<Produto>();
+		List<Produto> produtos = produtoDAO.listar();
 		produtos.add(new Produto("Nome do Produto de teste"
 				, "Descrição do produto de teste"));
 		return produtos;
 	}
 
 	public void alterar(String nome, String descricao, Integer id) {
-		System.out.println("Alterando produto");
+		produtoDAO.alterar(nome, descricao, id);
 	}
 }

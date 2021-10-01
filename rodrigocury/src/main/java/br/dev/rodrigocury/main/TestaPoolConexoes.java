fc72d721d86b04a2e12774f90fc50707f87a0319
Rodrigo Cury <rodrigocury@ufu.br>
2021-10-01 14:43:47 -0300

    Update TestaPoolConexoes.java

--- rodrigocury/src/main/java/br/dev/rodrigocury/main/TestaPoolConexoes.java
+++ rodrigocury/src/main/java/br/dev/rodrigocury/main/TestaPoolConexoes.java
@@ -22,13 +22,8 @@ public class TestaPoolConexoes {
 		
 		// Não Abre as vinte
 		for (int i = 0; i < 20; i++) {
-			try {
-				Connection c = ConnectioFactory.create();
-				System.out.println("Conexao criada: #" + i);
-				
-			} catch (SQLException e) {
-				System.out.println("ERRO");
-			}
+			Connection c = ConnectioFactory.create();
+			System.out.println("Conexao criada: #" + i);
 		}
 		
 	}
